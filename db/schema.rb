f0648d204fdb0f7171f96c8b552a7fedfa49e436
vanessa massiot <vanessa.massiot@free.fr>
2016-04-22 11:02:14 +0200

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160418192749) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stores", force: :cascade do |t|
     t.string   "name"
     t.integer  "stars"
